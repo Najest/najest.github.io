73a88c2766e6b00282eaa768a6413e2e2c0488c1
Najest Yang <39191337+Najest@users.noreply.github.com>
2023-12-18 16:37:56 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,6 +38,52 @@ $(document).ready(function () {
 		console.error(err);
 	})
 
+	// var url = 'https://query.yahooapis.com/v1/public/yql' +
+	// '?q=' +encodeURIComponent('从 json 中选择*，其中 url=@url') +
+	// '&url=' +encodeURIComponent('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8')+
+	// '&format=json&callback=?';
+
+	/**
+	 * 获取 Bing 壁纸
+	 * 语义 YQL 已经无法提供服务了
+	 *改用JsonBird：https://bird.ioliu.cn/
+	 *
+	 */
+	var url = 'https://bird.ioliu.cn/v1 /?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
+	var imgUrls = JSON。解析(sessionStorage。getItem (“imgUrls”));< /span>
+	var 索引 = sessionStorage。getItem(“索引”);< /span>
+	var $面板 = $(;)'#panel'
+	if (imgUrls == null{) 
+		imgUrls = 新 数组( );
+		索引 = 0;
+		$.获取(url , 函数 (结果)  {
+			图像 = 结果。图像;
+			对于 (让 i =  0; i < 图像++{)i;长度.  
+				常量 项目 = 图像[ i];
+				imgUrls。推送(项目.url);
+			}
+			var imgUrl = imgUrls[ 索引];
+			var url = “https://www.bing.com” ;imgUrl+ 
+			$panel.css(“背景” , "url('" + url< /span> ;)"") 中心中心无重复 #666"+ 
+			$panel.css("背景-尺寸”, “封面”);
+			sessionStorage.setItem(“imgUrls”< /span>(< /span>;))imgUrls字符串化.JSON, 
+			sessionStorage.setItem(“索引”< /span>;)索引, 
+		});
+	} 其他 {
+		if (索引 == 7)
+			索引 = 0;
+		别的
+			索引++;
+		var imgUrl = imgUrls[ 索引];
+		var url = “https://www.bing.com” ;imgUrl+ 
+		$panel.css(“背景” , "url('" + url< /span> ;)"") 中心中心无重复 #666"+ 
+		$panel.css("背景-尺寸”, “封面”);
+		sessionStorage.setItem(“索引”< /span>;)索引, 
+	}
+
+	$(“.iUp”)。 每个(函数 (i , e) {
+		iUp。向上(e );
+
 });
 
 $('.btn-mobile-menu__icon').click(function () {
